cc5c7d680be0057c3ec5ce411c44eee3796bd079
GaintStone <68771364+sen-gu@users.noreply.github.com>
2021-09-12 12:53:18 +0800

    Add dates and some guide info

--- include/head.h
+++ include/head.h
@@ -3,9 +3,9 @@
 #include<stdio.h>
 #include<string>
 #include"getpow4k.h"
-#define fileread1 "solexa_100_170_1.fa"
-#define fileread2 "solexa_100_170_2.fa"
-#define file_yssj "data.dna"
+#define fileread1 "../Data/solexa_100_170_1.fa"
+#define fileread2 "../Data/solexa_100_170_2.fa"
+#define file_yssj "../Data/data.dna"
 #define kmer_id_inall  DNA_NUM*(100 - kk+1) + kmer_ID
 void OutPut(unsigned int address);
 void OutPut(unsigned int DNA_num, unsigned int kmer_num);
